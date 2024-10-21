26e6cb9f57e683f107cf653a1cb63d3c487f7f92
Name <email@example.ru>
2024-10-21 16:35:07 +0530

    add info in views.py

--- pereval/app_pereval/views.py
+++ pereval/app_pereval/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import *
+from .models import *
+
+
+class AppUserViewSet(viewsets.ModelViewSet):
+    queryset = AppUser.objects.all()
+    serializer_class = AppUserSerializer
+    filterset_fields = ['email', 'name', 'surname', 'patronymic']
+
+
+class CoordsViewSet(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class PerevalViewSet(viewsets.ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ('user__email',)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = PerevalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    'status': status.HTTP_200_OK,
+                    'message': 'Выполнено успешно',
+                    'id': serializer.data['id'],
+                }
+            )
+
+        if status.HTTP_400_BAD_REQUEST:
+            return Response(
+                {
+                    'status': status.HTTP_400_BAD_REQUEST,
+                    'message': 'Некорректный запрос серверу',
+                    'id': None,
+                }
+            )
+
+        if status.HTTP_500_INTERNAL_SERVER_ERROR:
+            return Response(
+                {
+                    'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    'message': 'Ошибка сервера',
+                    'id': None,
+                }
+            )
+
+    def update(self, request, *args, **kwargs):
+        pereval = self.get_object()
+        if pereval.status == 'new':
+            serializer = PerevalSerializer(pereval, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'state': '1',
+                        'message': 'Успешно отредактировано'
+                    }
+                )
+            else:
+                return Response(
+                    {
+                        'state': '0',
+                        'message': serializer.errors
+                    }
+                )
+        else:
+            return Response(
+                {
+                    'state': '0',
+                    'message': f'Текущий статус: {pereval.get_status_display()} — редактирование запрещено.'
+                }
+            )
